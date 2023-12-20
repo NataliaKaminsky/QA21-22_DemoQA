12381cd4bc65b5b401b636964f41ae1255c79726
NataliaKaminsky <124137357+NataliaKaminsky@users.noreply.github.com>
2023-12-20 13:37:08 +0200

    added AppManager, tests

---
+++ src/main/java/manager/HelperBase.java
@@ -0,0 +1,12 @@
+package manager;
+
+import org.openqa.selenium.By;
+
+public interface HelperBase extends AppManager{
+    default void clickBase(By locator){
+        driver.findElement(locator).click();
+    }
+
+
+
+}
---
+++ src/main/java/manager/HelperStudent.java
@@ -0,0 +1,8 @@
+package manager;
+
+public interface HelperStudent extends HelperBase{
+
+
+
+
+}
